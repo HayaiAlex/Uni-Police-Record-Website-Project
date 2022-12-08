2aab96fe23d2b1401596d36b3cb65d7fcad39797
HayaiAlex <alexadams2084@gamil.com>
2022-12-08 18:34:00 +0000

    Added path config to php database connection

--- backend/protected/database.php
+++ backend/protected/database.php
@@ -2,15 +2,32 @@
 
 header('Content-Type: application/json');
 
-try {
-    $userName = 'root';
+// First find where we are and use the appropriate database information
+$directory = getcwd();
+$path = explode("backend",$directory)[0];
+
+$username = NULL;
+$password = NULL;
+$connection = NULL;
+
+if ($path == "C:\\xampp\\htdocs\\") {
+    $username = 'root';
     $password = '';
     $connection = 'mysql:host=localhost; dbname=police; charset=utf8';
+} elseif ($path == "\\h\\public_html\\police") {
+    $username = 'psxaa48_police';
+    $password = 'AMNPJF';
+    $connection = 'mysql:host=mysql.cs.nott.ac.uk; dbname=police; charset=utf8';
+}
+
+
+// Create a database connection
+try {
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // enables try-catch of SQL statements
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // outputs associative array which is convertable to JSON
     ];
-    $db = new PDO( $connection, $userName, $password, $options);
+    $db = new PDO($connection, $username, $password, $options);
 
 
 } catch(PDOException $ex) {
